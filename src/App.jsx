27c8c4a69ac1b152f49b436af8056e081f031ce0
Helena <hgonzalezg18@gmail.com>
2024-03-05 16:33:30 +0100

    exercises 2 part 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,48 @@ const Header = (props) => {
   );
 };
 
-const Content = (props) => {
+const Part1 = (props) => {
   return (
-  <>
     <p>
       {props.part1} {props.exercises1}
     </p>
+  );
+};
+
+const Part2 = (props) => {
+  return (
     <p>
       {props.part2} {props.exercises2}
     </p>
+  );
+};
+
+const Part3 = (props) => {
+  return (
     <p>
       {props.part3} {props.exercises3}
     </p>
-  </>
-  )
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part1 part1={props.part1} exercises1={props.exercises1} />
+      <Part2 part2={props.part2} exercises2={props.exercises2} />
+      <Part3 part3={props.part3} exercises3={props.exercises3} />
+    </>
+  );
 };
 
 const Total = (props) => {
   return (
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
-}
+    <p>
+      Number of exercises{' '}
+      {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
 
 const App = () => {
   const course = 'Half Stack application development';
@@ -40,15 +61,18 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
       />
-      <Total 
-      exercises1={exercises1}
-      exercises2={exercises2}
-      exercises3={exercises3}
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
       />
     </div>
   );
