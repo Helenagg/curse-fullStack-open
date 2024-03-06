82e614e47f437bae7e1c120b9667028c03d4f950
Helena <hgonzalezg18@gmail.com>
2024-03-06 18:55:54 +0100

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,33 @@
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  );
-};
+import { useState } from 'react';
 
-const Part1 = (props) => {
-  return (
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-  );
-};
+const Display = ({ counter }) => <div>{counter}</div>;
 
-const Part2 = (props) => {
-  return (
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-  );
-};
-
-const Part3 = (props) => {
-  return (
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <>
-      <Part1 part1={props.part1} exercises1={props.exercises1} />
-      <Part2 part2={props.part2} exercises2={props.exercises2} />
-      <Part3 part3={props.part3} exercises3={props.exercises3} />
-    </>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises{' '}
-      {props.exercises1 + props.exercises2 + props.exercises3}
-    </p>
-  );
-};
+const Button = ({onSmash, text}) => <button onClick={onSmash}>{text}</button>;
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [counter, setCounter] = useState(0);
+  console.log('rendering with counter value', counter);
 
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter);
+    setCounter(counter + 1);
+  };
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter);
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter);
+    setCounter(0);
+  };
   return (
     <div>
-      <Header course={course.name} />
-      <Content
-        part1={course.parts[0].name}
-        exercises1={course.parts[0].exercises}
-        part2={course.parts[1].name}
-        exercises2={course.parts[1].exercises}
-        part3={course.parts[2].name}
-        exercises3={course.parts[2].exercises}
-      />
-      <Total
-        exercises1={course.parts[0].exercises}
-        exercises2={course.parts[1].exercises}
-        exercises3={course.parts[2].exercises}
-      />
+      <Display counter={counter} />
+      <Button onSmash={increaseByOne} text='plus' />
+      <Button onSmash={decreaseByOne} text='minus' />
+      <Button onSmash={setToZero} text='reset' />
     </div>
   );
 };
