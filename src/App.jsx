ecc71dc8d808a604a002f03c5f1e4881330dd0cd
Helena <hgonzalezg18@gmail.com>
2024-03-11 17:22:21 +0100

    created left and right buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,33 @@
-
-
 import { useState } from 'react';
 
-const Display = ({ counter }) => <div>{counter}</div>;
+// const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = ({onSmash, text}) => <button onClick={onSmash}>{text}</button>;
+// const Button = ({onSmash, text}) => <button onClick={onSmash}>{text}</button>;
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log('rendering with counter value', counter);
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => {
-    console.log('increasing, value before', counter);
-    setCounter(counter + 1);
-  };
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
 
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter);
-    setCounter(counter - 1);
-  };
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter);
-    setCounter(0);
-  };
   return (
     <div>
-      <Display counter={counter} />
-      <Button onSmash={increaseByOne} text='plus' />
-      <Button onSmash={decreaseByOne} text='minus' />
-      <Button onSmash={setToZero} text='reset' />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
-  );
+  )
 };
 
 export default App;
