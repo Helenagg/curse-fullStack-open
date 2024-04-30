88d35252510257c716d35fc57f4d531417ea0b4b
Helena <hgonzalezg18@gmail.com>
2024-04-30 17:44:20 +0200

    desestructuracion

--- src/App.jsx
+++ src/App.jsx
@@ -27,77 +27,31 @@
 // };
 
 // export default App;
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  );
-};
+const Hello = ({ name, age }) => {
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.part} exercises={props.exercises} />
-    </>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <>
-      <p>
-        Number of exercises{' '}
-        {props.exercises1 + props.exercises2 + props.exercises3}
-      </p>
-    </>
-  );
-};
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
-      <Header course={course.name} />
-      {course.parts.map((item) => (
-        <>
-          <Content
-            part={item.name}
-            exercises={item.exercises} />
-        </>
-      ))}
-      <Total
-        exercises1={course.parts[0].exercises}
-        exercises2={course.parts[1].exercises}
-        exercises3={course.parts[2].exercises}
-      />
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
-  );
-};
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
 
 export default App;
