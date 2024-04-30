573aadcd430f5fa519cf102a005b25d29512ff5c
Helena <hgonzalezg18@gmail.com>
2024-04-30 18:06:09 +0200

    counter with click

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import { useState } from 'react';
 
+import { useState } from "react";
+
 // const App = () => {
 //   const [left, setLeft] = useState(0)
 //   const [right, setRight] = useState(0)
@@ -27,31 +29,21 @@
 // };
 
 // export default App;
-const Hello = ({ name, age }) => {
-
-  const bornYear = () => new Date().getFullYear() - age
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [counter, setCounter] = useState(0)
 
+  const handleClick = () => {
+    console.log('he clicado');
+  }
+  
   return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
+    <>
+    <div>{counter}</div>
+    <button onClick={() => setCounter(counter + 1)}>plus</button>
+    <button onClick={() => setCounter(0)}>zero</button>
+    </>
   )
 }
 
 export default App;
+
