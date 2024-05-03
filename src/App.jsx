89f3b300d306187bee7c83889b90682fb8a69af6
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-05-03 11:39:13 +0200

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,55 @@
-// import { useState } from 'react';
-
 import { useState } from 'react';
 
-// const App = () => {
-//   const [left, setLeft] = useState(0)
-//   const [right, setRight] = useState(0)
-//   const [allClicks, setAll] = useState([])
-
-//   const handleLeftClick = () => {
-//     setAll(allClicks.concat('L'))
-//     setLeft(left + 1)
-//   }
-
-//   const handleRightClick = () => {
-//     setAll(allClicks.concat('R'))
-//     setRight(right + 1)
-//   }
-
-//   return (
-//     <div>
-//       {left}
-//       <button onClick={handleLeftClick}>left</button>
-//       <button onClick={handleRightClick}>right</button>
-//       {right}
-//       <p>{allClicks.join(' ')}</p>
-//     </div>
-//   )
-// };
-
-// export default App;
-
-const Display = ({ counter }) => <div>{counter}</div>;
-
-const Button = ({ onSmash, text}) => <button onClick={onSmash}>{text}</button>
-
-const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0);
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
-    <>
-      <Display counter={counter} />
-      <Button onSmash={increaseByOne} text='plus' />
-      <Button onSmash={decreaseByOne} text='minus' />
-      <Button onSmash={setToZero} text='zero' />
-    </>
-  );
+    <div>
+      button press history: {allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState([])
+  
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    console.log('left after', left)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text={'left'} />
+      <Button handleClick={handleRightClick} text={'right'} />
+      {right}
+      <History allClicks={allClicks} />
+      <p>total: {total}</p>
+    </div>
+  )
 };
 
 export default App;
