ae57d2bff96e88971ba4dc876ee3a87525ed8960
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-05-06 09:54:20 +0200

    function that returns a function

--- src/App.jsx
+++ src/App.jsx
@@ -23,13 +23,12 @@ const App = () => {
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
   const [total, setTotal] = useState([])
-  
+  const [value, setValue] = useState(10)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
     const updatedLeft = left + 1
     setLeft(updatedLeft)
-    console.log('left after', left)
     setTotal(updatedLeft + right)
   }
 
@@ -40,14 +39,24 @@ const App = () => {
     setTotal(left + updatedRight)
   }
 
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)  // imprime el nuevo valor en la consola
+    setValue(newValue)
+  }
   return (
+    // <div>
+    //   {left}
+    //   <Button handleClick={hello('world')} text={'left'} />
+    //   <Button handleClick={handleRightClick} text={'right'} />
+    //   {right}
+    //   <History allClicks={allClicks} />
+    //   <p>total: {total}</p>
+    // </div>
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text={'left'} />
-      <Button handleClick={handleRightClick} text={'right'} />
-      {right}
-      <History allClicks={allClicks} />
-      <p>total: {total}</p>
+      {value}
+      <button onClick={() => setToValue(1000)}>thousand</button>
+      <button onClick={() => setToValue(0)}>reset</button>
+      <button onClick={() => setToValue(value + 1)}>increment</button>
     </div>
   )
 };
