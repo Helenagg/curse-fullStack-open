14e04653d3731d75d4db134e93d35444b0be4fad
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-05-06 10:48:04 +0200

    exercise 1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,38 @@
 import { useState } from 'react';
 
-const History = ({ allClicks }) => {
-  if (allClicks.length === 0) {
-    return (
-      <div>
-        The app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      button press history: {allClicks.join(' ')}
-    </div>
-  )
-}
-
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
-const Display = (props) => <div>{props.value}</div>
+const Display = (props) => <div>{props.text}</div>
+
+const Results = ({ text, result }) => <div>{text}{result}</div>
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState([])
-  const [value, setValue] = useState(10)
+  const [goodValue, setGoodValue] = useState(0)
+  const [neutralValue, setNeutralValue] = useState(0)
+  const [badValue, setBadValue] = useState(0)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-    setTotal(updatedLeft + right)
+  const handleGoodClick = () => {
+    setGoodValue(goodValue + 1)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updatedRight = right + 1
-    setRight(updatedRight)
-    setTotal(left + updatedRight)
+  const handleNeutralClick = () => {
+    setNeutralValue(neutralValue + 1)
   }
 
-  const setToValue = (newValue) => {
-    console.log('value now', newValue)  // imprime el nuevo valor en la consola
-    setValue(newValue)
+  const handleBadClick = () => {
+    setBadValue(badValue + 1)
   }
+
   return (
-    // <div>
-    //   {left}
-    //   <Button handleClick={handleLeftClick} text={'left'} />
-    //   <Button handleClick={handleRightClick} text={'right'} />
-    //   {right}
-    //   <History allClicks={allClicks} />
-    //   <p>total: {total}</p>
-    // </div>
     <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text='thousand' />
-      <Button handleClick={() => setToValue(0)} text='reset' />
-      <Button handleClick={() => setToValue(value + 1)} text='increment' />
+      <Display text={'Give feedback'} />
+      <Button handleClick={handleGoodClick} text='Good' />
+      <Button handleClick={handleNeutralClick} text='Neutral' />
+      <Button handleClick={handleBadClick} text='Bad' />
+      <Display text={'Statistics'} />
+      <Results text='Good: ' result={goodValue} />
+      <Results text='Neutral: ' result={neutralValue} />
+      <Results text='Bad: ' result={badValue} />
     </div>
   )
 };
