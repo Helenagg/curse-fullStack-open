15648ba715c346b563012c886ddee7a0402f15cf
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-05-06 12:05:50 +0200

    exercise 1.9

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,32 @@
 import { useState } from "react";
 
-// const Button = ({ handleClick, text }) => (
-//   <button onClick={handleClick}>{text}</button>
-// );
+const Statistics = (props) => (
+  <div>
+    {props.text}
+    {props.value}
+  </div>
+);
 
-// const Display = (props) => <div>{props.text}</div>;
+const OpenStatistics = (props) => {
+  if (!props.goodValue && !props.neutralValue && !props.badValue) {
+    return (
+      <div>
+        <p>no feedback given</p>
+      </div>
+    );
+  }
 
-// const Results = ({ text, result }) => <div>{text}{result}</div>
-
-// const Statistics = ({ good, neutral, bad }) => {
-
-//   const total = good + neutral + bad;
-
-//   const weightedTotal = good * 1 + neutral * 0 + bad * -1;
-//   const average = weightedTotal / total;
-
-//   const percentageGood = (good / total) * 100;
-
-//   return (
-//     <div>
-//       <Results text='Good: ' result={good} />
-//       <Results text='Neutral: ' result={neutral} />
-//       <Results text='Bad: ' result={bad} />
-//       <Results text='Total: ' result={total} />
-//       <Results text='Average: ' result={average ? average : 0} />
-//       <Results text='Positive: ' result={percentageGood ? percentageGood + "%" : 0} />
-//     </div>
-//   );
-// };
-
- {/* <Display text={"Give feedback"} /> */}
-      {/* <Button handleClick={handleGoodClick} text='Good' /> */}
-      {/* <Button handleClick={handleNeutralClick} text='Neutral' /> */}
-      {/* <Button handleClick={handleBadClick} text='Bad' /> */}
-      {/* <Display text={"Statistics"} /> */}
-      {/* <Statistics good={goodValue} neutral={neutralValue} bad={badValue} /> */}
-
-const Statistics = (props) => <div>{props.text}{props.value}</div>
+  return (
+    <div>
+      <Statistics text='Good: ' value={props.goodValue} />
+      <Statistics text='Neutral: ' value={props.neutralValue} />
+      <Statistics text='Bad: ' value={props.badValue} />
+      <Statistics text='Total: ' value={props.total} />
+      <Statistics text='Average: ' value={props.average} />
+      <Statistics text='Percentage: ' value={props.percentageGood} />
+    </div>
+  );
+};
 
 const App = () => {
   const [goodValue, setGoodValue] = useState(0);
@@ -53,7 +43,7 @@ const App = () => {
 
   const handleBadClick = () => {
     setBadValue(badValue + 1);
-  };  
+  };
 
   const total = goodValue + neutralValue + badValue;
 
@@ -73,12 +63,14 @@ const App = () => {
       <div>
         <p>Statistics</p>
       </div>
-      <Statistics text='Good: ' value={goodValue} />
-      <Statistics text='Neutral: ' value={neutralValue} />
-      <Statistics text='Bad: ' value={badValue} />
-      <Statistics text='Total: ' value={total} />
-      <Statistics text='Average: ' value={average} />
-      <Statistics text='Percentage: ' value={percentageGood} />
+      <OpenStatistics 
+        goodValue={goodValue}
+        neutralValue={neutralValue}
+        badValue={badValue}
+        total={total}
+        average={average}
+        percentageGood={percentageGood}
+      />
     </div>
   );
 };
