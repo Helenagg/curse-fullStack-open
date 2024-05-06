a64c3325b5395ffb74c74ff5060bda4e63852ba5
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-05-06 11:12:29 +0200

    exercise 1.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,63 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
-const Display = (props) => <div>{props.text}</div>
+const Display = (props) => <div>{props.text}</div>;
 
 const Results = ({ text, result }) => <div>{text}{result}</div>
 
-const App = () => {
-  const [goodValue, setGoodValue] = useState(0)
-  const [neutralValue, setNeutralValue] = useState(0)
-  const [badValue, setBadValue] = useState(0)
+const Statistics = ({ good, neutral, bad }) => {
 
-  const handleGoodClick = () => {
-    setGoodValue(goodValue + 1)
-  }
+  const total = good + neutral + bad;
 
-  const handleNeutralClick = () => {
-    setNeutralValue(neutralValue + 1)
-  }
-
-  const handleBadClick = () => {
-    setBadValue(badValue + 1)
-  }
-
-  const total = goodValue + neutralValue + badValue
-
-  const weightedTotal = (goodValue * 1) + (neutralValue * 0) + (badValue * -1);
+  const weightedTotal = good * 1 + neutral * 0 + bad * -1;
   const average = weightedTotal / total;
 
-  const percentageGood = (goodValue / total) * 100 
+  const percentageGood = (good / total) * 100;
 
   return (
     <div>
-      <Display text={'Give feedback'} />
+      <Results text='Good: ' result={good} />
+      <Results text='Neutral: ' result={neutral} />
+      <Results text='Bad: ' result={bad} />
+      <Results text='Total: ' result={total} />
+      <Results text='Average: ' result={average ? average : 0} />
+      <Results text='Positive: ' result={percentageGood ? percentageGood + "%" : 0} />
+    </div>
+  );
+};
+
+const App = () => {
+  const [goodValue, setGoodValue] = useState(0);
+  const [neutralValue, setNeutralValue] = useState(0);
+  const [badValue, setBadValue] = useState(0);
+
+  const handleGoodClick = () => {
+    setGoodValue(goodValue + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutralValue(neutralValue + 1);
+  };
+
+  const handleBadClick = () => {
+    setBadValue(badValue + 1);
+  };
+
+  
+
+  return (
+    <div>
+      <Display text={"Give feedback"} />
       <Button handleClick={handleGoodClick} text='Good' />
       <Button handleClick={handleNeutralClick} text='Neutral' />
       <Button handleClick={handleBadClick} text='Bad' />
-      <Display text={'Statistics'} />
-      <Results text='Good: ' result={goodValue} />
-      <Results text='Neutral: ' result={neutralValue} />
-      <Results text='Bad: ' result={badValue} />
-      <Results text='Total: ' result={total} />
-      <Results text='Average: ' result={average} />
-      <Results text='Positive: ' result={percentageGood + '%' } />
+      <Display text={"Statistics"} />
+      <Statistics good={goodValue} neutral={neutralValue} bad={badValue} />
     </div>
-  )
+  );
 };
 
 export default App;
