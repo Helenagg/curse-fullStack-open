cefb25006e3ba4401b6bcc2fbea69e3ece011b6f
Helena Gonzalez <helenagonzalez@iMac-de-Helena.local>
2024-05-27 18:32:06 +0200

    exercise part 2.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
-import Note from "./components/Note";
+const Header = ({ title }) => {
+  return <h1>{title}</h1>;
+};
 
-const App = ({ notes }) => {
-
+const Course = ({ courseName, exercise }) => {
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note key={note.id} note={note}/>
-        ))}
-      </ul>
-    </div>
+    <>
+      <p>
+        {courseName} {exercise}
+      </p>
+    </>
   );
 };
 
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Header title={course.name} />
+      {course.parts.map((part) => (
+        <Course courseName={part.name} exercise={part.exercises} />
+      ))}
+    </>
+  );
+};
 
 export default App;
